1f9548770e3db48652859581ab080f1a1eb826b4
vitaliiryzhenkous <vitaliiryzhenkous@gmail.com>
2017-08-12 20:14:18 +0300

    fix bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-
 import Hero from '../Hero/Hero';
-=======
 import styles from './App.css';
->>>>>>> cbdcc54bf20a671a2c288d99aa5a019107a03d9b
 import Header from '../Header/Header';
 
 class App extends React.Component {
@@ -16,12 +12,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
                 <Hero/>
                 <Header/>
-=======
-                <Header />
->>>>>>> cbdcc54bf20a671a2c288d99aa5a019107a03d9b
             </div>
         )
     }
