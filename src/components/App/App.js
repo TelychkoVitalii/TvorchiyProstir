53f4db19199c2afd426a79372840cd7f444f5362
TelychkoVitalii <newme1989.5@gmail.com>
2017-08-12 18:21:39 +0300

    fixed folder structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Header from '../Header/Header';
 
 class App extends React.Component {
@@ -11,7 +10,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header/>
+                <Header />
             </div>
         )
     }
