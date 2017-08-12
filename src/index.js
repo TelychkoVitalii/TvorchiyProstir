4e510ccdc4562ddc8b9f453466b447ad697fc49b
TelychkoVitalii <newme1989.5@gmail.com>
2017-08-12 17:34:48 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppComponent from './App/App';
+
+ReactDOM.render(
+    <AppComponent/>,
+    document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+    devtool: 'eval',
+
+    entry: [
+        path.resolve('src/index.js'),
+    ],
+
+    output: {
+        path: path.resolve('build'),
+        filename: 'static/js/bundle.js',
+        publicPath: '/',
+    },
+
+    devServer: {
+        port: 3333
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: path.resolve('src'),
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: true, // Note. This prop enables CSS modules.
+                        },
+                    }
+                ]
+            }
+        ]
+    },
+    
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve('src/index.html'),
+        }),
+        
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
+};
