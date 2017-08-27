35e1af6adb406b6847cde3b1b9ffee3b06441019
TelychkoVitalii <newme1989.5@gmail.com>
2017-08-27 18:34:41 +0300

    little cha-cha-cha team commit

--- src/components/Main/About/Team/Team.js
+++ src/components/Main/About/Team/Team.js
@@ -7,13 +7,41 @@ class Team extends React.Component {
     render() {
         return (
             <div>
+            <h2 className={styles.titleTeam}>Давайте знакомиться!</h2>
+            <h3 className={styles.titleTeam}>Мы- команда Tvorchiy Prostir и по совместительству лучшие друзья.</h3>
                <div className={styles.oneRow}>
-                    <p>1</p>
-                    <p>2</p>
+               <figure>
+                    <img src='https://s2.postimg.org/j97ueewg9/IMG_7856.jpg' 
+                         className={styles.photoImage}
+                         alt='photo_1'/>
+                    // <figcaption>
+                    //   Наше сильное плечо, которое умеет управлять любыми транспортными средствами, собственными руками делать пятиметровые вывески и выдерживать все, что происходит вокруг в части женского коллектива. В команде отвечает за юридическую и техническую сторону.
+                    // </figcaption>
+              </figure>
+              <figure>
+                    <img src='https://s2.postimg.org/gwzugtjux/IMG_7875.jpg' 
+                         className={styles.photoImage}
+                         alt='photo_2'/>
+              </figure>
                </div>
                <div className={styles.oneRow}>
-                    <p>1</p>
-                    <p>2</p>
+               <figure>
+                    <img src='https://s2.postimg.org/e5l63yaq1/IMG_7912.jpg'
+                         className={styles.photoImage} 
+                         alt='photo_3'/>
+               </figure>
+              <figure>
+                    <img src='https://s2.postimg.org/t2y8ipdc9/IMG_7897.jpg' 
+                         className={styles.photoImage}
+                         alt='photo_4'/>
+               </figure>
+               </div>
+               <div className={styles.oneRow}>
+               <figure>
+                    <img src='https://s2.postimg.org/mqj38vaa1/IMG_7877.jpg' 
+                         className={styles.photoImage}
+                         alt='photo_5'/>
+               </figure>
                </div>
             </div>
         )
